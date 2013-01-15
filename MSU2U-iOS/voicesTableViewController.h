58b77c76ae6872aef961c34ccc19da318d6962e0
Matthew Farmer <matthew.farmer@mwsu.edu>
2013-01-15 12:50:20 -0600

    Add starting Voices PDF Table before implementing code to download PDF files

---
+++ MSU2U-iOS/voicesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  voicesTableViewController.h
+//  MSU2U-iOS
+//
+//  Created by Matthew Farmer on 1/15/13.
+//  Copyright (c) 2013 Matthew Farmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface voicesTableViewController : UITableViewController{
+    NSArray * voicesYear;
+}
+
+@end
---
+++ MSU2U-iOS/voicesTableViewController.m
@@ -0,0 +1,63 @@
+//
+//  voicesTableViewController.m
+//  MSU2U-iOS
+//
+//  Created by Matthew Farmer on 1/15/13.
+//  Copyright (c) 2013 Matthew Farmer. All rights reserved.
+//
+
+#import "voicesTableViewController.h"
+
+@interface voicesTableViewController ()
+
+@end
+
+@implementation voicesTableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    voicesYear = [[NSArray alloc] initWithObjects:@"1977",@"1978",@"1979",@"1980",@"1990",@"1991",@"1992",@"1994",@"2010",@"2011", nil];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [voicesYear count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"voicesCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    cell.textLabel.text = [voicesYear objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = @"[ EXAMPLE ROW ONLY ]";
+    
+    //Example: 1977.png
+    cell.imageView.image = [UIImage imageNamed:[[voicesYear objectAtIndex:indexPath.row]stringByAppendingString:@".png"]];
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
