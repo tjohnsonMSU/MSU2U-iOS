95595285280878a42641b74eaae4bb71646b695e
Romando Garcia <admin@Romando-Garcias-MacBook-Pro.local>
2013-11-12 17:03:35 -0600

    calendarViewController

---
+++ MSU2U-iOS/CalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalendarViewController.h
+//  MSU2U-iOS
+//
+//  Created by Romando Garcia on 11/12/13.
+//  Copyright (c) 2013 Matthew Farmer. All rights reserved.
+//
+
+#import "tabBarViewController.h"
+
+@interface CalendarViewController : tabBarViewController
+
+@end
---
+++ MSU2U-iOS/CalendarViewController.m
@@ -0,0 +1,38 @@
+//
+//  CalendarViewController.m
+//  MSU2U-iOS
+//
+//  Created by Romando Garcia on 11/12/13.
+//  Copyright (c) 2013 Matthew Farmer. All rights reserved.
+//
+
+#import "CalendarViewController.h"
+
+@interface CalendarViewController ()
+
+@end
+
+@implementation CalendarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
